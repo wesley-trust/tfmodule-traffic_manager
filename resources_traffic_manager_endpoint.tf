4ae995fa8b9630c06a9b88c75e624941abdace23
Wesley Trust <18400710+wesley-trust@users.noreply.github.com>
2022-06-01 16:52:50 +0100

    Remove RG reference

--- resources_traffic_manager_endpoint.tf
+++ resources_traffic_manager_endpoint.tf
@@ -2,7 +2,6 @@
 resource "azurerm_traffic_manager_azure_endpoint" "traffic_manager_endpoint" {
   for_each            = var.resource_traffic_manager_endpoint_type == "Azure_Endpoint" ? toset(var.resource_traffic_manager_endpoint_locations) : []
   name                = "${local.service_environment_prefix}-${replace(each.value, "/[a-z[:space:]]/", "")}-${var.service_deployment}-${var.resource_name}"
-  resource_group_name = module.resource_group.name
   profile_id          = azurerm_traffic_manager_profile.traffic_manager_profile.id
   target_resource_id  = var.resource_traffic_manager_endpoints[each.value].public_ip_id
   weight              = 100

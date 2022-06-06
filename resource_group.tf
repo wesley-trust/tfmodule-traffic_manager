fb76859747ec72970d2cce432e859013a12ba871
Wesley Trust <18400710+wesley-trust@users.noreply.github.com>
2022-06-06 21:02:17 +0100

    Update module version reference

--- resource_group.tf
+++ resource_group.tf
@@ -1,6 +1,6 @@
 # Define resource group for resource
 module "resource_group" {
-  source              = "github.com/wesley-trust/tfmodule-resource_group?ref=v1"
+  source              = "github.com/wesley-trust/tfmodule-resource_group?ref=v1-resource_group"
   service_environment = var.service_environment
   service_name        = var.service_name
   service_location    = var.service_location
